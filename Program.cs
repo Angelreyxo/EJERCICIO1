b6fd542f30ad62e45c47f93c6b3b50f777e824d3
Angel Reynoso <angelreynosoduarte@gmail.com>
2021-04-26 14:19:04 -0400

    comentario

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P3_Tarea1
+{
+    class Program
+    {
+        //ANGEL E. REYNOSO DUARTE 2020-03508
+        static void Main(string[] args)
+        {
+            claUapa profe1 = new claUapa();
+            profe1.id = 1;
+            profe1.nombre = "Pedro";
+            profe1.apPaterno = "Rodriguez";
+            profe1.cargo = "Maestro de Fisica";
+            profe1.sueldo = 35000;
+
+            Console.WriteLine( profe1.mostrarDatos());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ claUapa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P3_Tarea1
+{   //ANGEL E. REYNOSO DUARTE 2020-03508
+    public class claUapa
+    {
+        public int id;
+        public string nombre;
+        public string apPaterno;
+        public string cargo;
+        public double sueldo;
+
+        //METODO PARA MOSTRAR LOS DATOS 
+        public string mostrarDatos()
+        {
+           return "ID: " +this.id+ "\nNombre y apellido: "+ this.nombre +" "
+                   +this.apPaterno+"\nCargo: "+this.cargo +"\nSueldo: "+this.sueldo;
+        } 
+    }
+}
